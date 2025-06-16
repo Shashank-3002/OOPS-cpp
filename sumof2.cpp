188f98a72924ee4a9d8d5001755d555d7725c2d7
Shashank Naragundmath <shashanknaragundmath@gmail.com>
2025-06-16 19:24:14 +0530

    Sum of 2 numbers

---
+++ sumof2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the values of a and b: " <<endl; 
+    cin>> a,b; <<endl;
+    int sum=a+b;
+    cout<<"Sum= "<<sum <endl;
+    return 0;
+}
